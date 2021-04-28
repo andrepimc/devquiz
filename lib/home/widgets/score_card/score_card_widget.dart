ea7c804d5c1f01543a84c200493692f98a03de45
André Pimentel <andrepc690@gmail.com>
2021-04-27 23:44:15 -0300

    aa

--- lib/home/widgets/score_card/score_card_widget.dart
+++ lib/home/widgets/score_card/score_card_widget.dart
@@ -1,4 +1,4 @@
-import 'package:DevQuiz/core/app_colors.dart';
+
 import 'package:DevQuiz/core/app_text_styles.dart';
 import 'package:DevQuiz/home/widgets/chart/chart_widget.dart';
 import 'package:flutter/material.dart';
