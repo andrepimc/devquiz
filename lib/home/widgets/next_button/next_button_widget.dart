5e728052e2b26d1067d93a19693a97dc04705655
André Pimentel <andrepc690@gmail.com>
2021-05-02 17:05:46 -0300

    next button

---
+++ lib/home/widgets/next_button/next_button_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class NextButtonWidget extends StatelessWidget {
+  final String label;
+  const NextButtonWidget({Key? key,required this.label}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(onPressed: (){},child: 
+    Text(label)
+    ,);
+  }
+}
